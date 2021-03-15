f1520ea0e2161d1a2bbad26b3435c336594e7f4e
Maciej U <urbanskimac@gmail.com>
2021-03-15 20:02:45 +0100

    Client can pick up elevator

--- main.c
+++ main.c
@@ -5,10 +5,15 @@
 
 #define MAX_AMOUNT_OF_ELEVATORS  16
 
+const uint32_t max_floor = 10;
+
 int currAmountOfElevators = 0;
 
 typedef struct Person{
+    uint8_t isWaiting;
+    uint8_t activePlace;
     uint32_t floor;
+    uint32_t objectiveFloor;
 }Person;
 
 //data which represents particular elevator
@@ -16,47 +21,118 @@ typedef struct Elevator{
     uint32_t ID;
     uint32_t curr_floor;
     uint32_t objective_floor;
+    uint8_t empty;
+    Person people[5];
+    uint8_t actualAmountOfPeople;
 }Elevator;
 
 
 
 //table with elevators
-Elevator elevators[MAX_AMOUNT_OF_ELEVATORS];
 
-Elevator pickUp(int floor, int dir){
+
+Elevator pickUp(int floor, int dir, Elevator *elevators, int *currAm){
+ /*
+    for(int i = 0 ; i < *currAm; i++) {
+
+        if (elevators[i].actualAmountOfPeople <= 5) {
+
+            //check if elevator which may have the same route, can take the person
+            if ((elevators[i].objective_floor > elevators[i].curr_floor && floor > elevators[i].curr_floor &&
+                 floor < elevators[i].objective_floor) ||
+                (elevators[i].objective_floor < elevators[i].curr_floor && floor < elevators[i].curr_floor &&
+                 floor > elevators[i].objective_floor)) {
+
+                Person p;
+                p.floor = floor;
+                p.objectiveFloor = floor + dir;
+                p.isWaiting = 1;
+                elevators[i].actualAmountOfPeople++;
+                elevators[i].people[elevators[i].actualAmountOfPeople] = p;
+
+                return elevators[i];
+            }
+        }
+    }*/
+
+ //TODO making signing clients who are waiting
+
+ for(int i = 0; i<MAX_AMOUNT_OF_ELEVATORS; i++){
+
+ }
+
+    Person p1;
+    p1.isWaiting = 1;
+    p1.floor = floor;
+    p1.objectiveFloor = floor + dir;
+
     Elevator e;
-    e.ID = 0;
+    e.ID = *currAm;
     e.objective_floor = floor;
     e.curr_floor = 0;
-    int flag = 0;
+    e.empty = 1;
+    e.actualAmountOfPeople = 0;
+
+    //person who picked up elevator and is waiting for it
+    e.people[e.actualAmountOfPeople] = p1;
+
     int i = 0;
+
     //TODO przywaołanie przez pojedynczego klienta, co jesli chcemy obsłuzyc jedna winda wiecej klientów?
     //TODO rozwazyc przywołania innych klientów którzy są po drodze i moze i ich mozna obsłuzyc (program wydajniejszy)
-    for(i = 0 ; i < currAmountOfElevators;i++){
-        if(elevators[i].curr_floor == elevators[i].objective_floor){
-            elevators[i].objective_floor = floor;
-            return elevators[i];
+    if(floor + dir < max_floor && floor + dir >= 0) {
+        for (i = 0; i < *currAm; i++) {
+            if (elevators[i].curr_floor == elevators[i].objective_floor) {
+
+                //bład
+                elevators[i].objective_floor = floor;
+                return elevators[i];
+            }
         }
     }
-    currAmountOfElevators++;
+    (*currAm)++;
     return e;
 }
 
-void update(Elevator e){
+void update(Elevator e, Elevator elevators[]){
     elevators[e.ID] = e;
 }
 
-void step(){
+void step(Elevator elevators[], int *currAm){
     int i = 0;
+
     //executing one step of simulation
-    for(i = 0 ; i < MAX_AMOUNT_OF_ELEVATORS; i++){
-        if(elevators[i].curr_floor > elevators[i].objective_floor){
+    for(i = 0 ; i < *currAm; i++){
+
+        if(elevators[i].curr_floor == elevators[i].objective_floor){
+
+
+            for(int j = 0; j < 5; j++){
+
+                //getting off the elevator
+                if(elevators[i].people[j].activePlace == 1){
+                    elevators[i].people[j].activePlace = 0;
+                }
+
+                else if(elevators[i].people[j].activePlace == 0){
+                    //getting on the elevator
+                    //person went into elevator
+                    elevators[i].people[j].activePlace = 1;
+
+                    //assignment objective floor of elevator after person went into the elevator
+                    elevators[i].objective_floor = elevators[i].people[elevators[i].actualAmountOfPeople].objectiveFloor;
+                }
+            }
+
+        }
+        else if(elevators[i].curr_floor > elevators[i].objective_floor){
+            //move of elevator
             elevators[i].curr_floor--;
         }
         else if(elevators[i].curr_floor < elevators[i].objective_floor){
+            //move of elevator
             elevators[i].curr_floor++;
         }
-        //if curr_floor == objective_floor  --> the elevator is waiting for client
     }
 }
 
@@ -64,16 +140,36 @@ Elevator* status(){
     //TODO how to return status
 }
 
+void init(Elevator elevators[]){
+    for(int i = 0; i<MAX_AMOUNT_OF_ELEVATORS-1; i++){
+        elevators[i].curr_floor = 0;
+        for(int j=0; j<5;j++){
+            elevators[i].people[j].activePlace = 1;
+        }
+    }
+}
+
 
 int main() {
+    Elevator elevators[MAX_AMOUNT_OF_ELEVATORS];
 
+    init(elevators);
+
+    int i = 3, j =0;
+    int *currAm = &j;
+    Elevator e = pickUp(4,-2,elevators,currAm);
+    update(e,elevators);
     while(1){
-        Elevator e = pickUp(10,-2);
 
-        update(e);
 
-        step(elevators);
-        Elevator* elevators = status();
+
+
+        step(elevators, currAm);
+
+        //to show message
+        //Elevator* elevators = status();
+        i++;
+        //currAmountOfElevators++;
     }
 
     return 0;
